abedf5cf2eb777dc98ad68fd51949714426ca4d1
saadmgit <74968320+saadmgit@users.noreply.github.com>
2020-11-24 18:43:02 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+def sum_nums(num1, num2):
+    return num1 + num2
+
+
+SumValue = sum_nums(5, 10)
+print(sum)
---
+++ task3.py
@@ -0,0 +1,8 @@
+
+output_dict = {}
+input_num = 8
+
+for x in range(1, input_num+1):
+    output_dict[x] = x*x
+
+print(output_dict)
